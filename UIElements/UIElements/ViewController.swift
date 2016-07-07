ea3d0ad9a50ca803b012be29b4fe3fcc2c6d436f
hdinhof1 <hdinhof1@binghamton.edu>
2016-07-07 14:51:25 -0400

    Done

--- UIElements/UIElements/ViewController.swift
+++ UIElements/UIElements/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: UIProgressView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewDidAppear(animated: Bool) {
+        messAroundWithProgressView()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func messAroundWithProgressView() {
+      //  progressView.setProgress(0.5, animated: true)
+        print(progressView.description)
+        progressView.setProgress(0.94, animated: true)
+    }
 }
 
